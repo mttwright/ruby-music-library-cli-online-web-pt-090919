8f76585146d05eab6ab7c83da76f6a9c15c0fc3f
virtual-frame-7789 <mttwright@gmail.com>
2019-10-20 21:08:13 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -6,7 +6,12 @@ module Concerns::Findable
     end
   end
   
-  def find_or_create_by_name(name)
-    
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create(name)
+    end
+  end
   
 end
